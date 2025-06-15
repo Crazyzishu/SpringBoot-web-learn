60115d490aa8db593d9b7d4bd0c2f05a1175d96f
zishu <129256950+Crazyzishu@users.noreply.github.com>
2025-06-15 21:53:59 +0800

    update

---
+++ tlias-web-management/src/main/java/org/zishu/config/WebConfig.java
@@ -0,0 +1,29 @@
+package org.zishu.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zishu.interceptor.DemoInterceptor;
+import org.zishu.interceptor.TokenInterceptor;
+
+/**
+ * 配置类
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Autowired
+//    private DemoInterceptor demoInterceptor;
+
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/**")//拦截所有请求
+                .excludePathPatterns("/login");//不拦截哪些请求
+    }
+}
--- tlias-web-management/src/main/java/org/zishu/interceptor/TokenInterceptor.java
+++ tlias-web-management/src/main/java/org/zishu/interceptor/TokenInterceptor.java
@@ -1,30 +1,27 @@
-package org.zishu.filter;
+package org.zishu.interceptor;
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.zishu.utils.JwtUtils;
 
-import java.io.IOException;
-
+/**
+ *令牌校验的拦截器
+ */
+@Component
 @Slf4j
-//@WebFilter(urlPatterns = "/*")
-public class TokenFilter implements Filter {
+public class TokenInterceptor implements HandlerInterceptor {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //1.获取请求路径
         String requestURI = request.getRequestURI();// 相比于URL不包括协议,ip,端口,只包括:/employee/login
 
         //2.判断是否是登录请求,如果路径中包含 /login,说明是登陆操作->放行
         if(requestURI.contains("/login")){
             log.info("登录请求,放行");
-            filterChain.doFilter(request,response);
-            return;
+            return true;
         }
 
         //3.获取请求头中的token
@@ -33,8 +30,8 @@ public class TokenFilter implements Filter {
         //4.判断token是否存在,如果不存在,说明用户没有登陆,返回错误信息(响应401状态码)
         if(token == null || token.isEmpty()){//isEmpty -> 用来判断是否为空字符串
             log.info("令牌为空,响应401状态码");
-             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-             return;
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);//401状态码
+            return false;
         }
 
         //5.如果token存在,校验令牌,如果校验失败 -> 返回错误信息(响应401状态码)
@@ -43,11 +40,11 @@ public class TokenFilter implements Filter {
         } catch (Exception e){
             log.info("令牌非法,响应401状态码");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return;
+            return false;
         }
 
         //6.校验通过,放行
         log.info("令牌合法,放行");
-        filterChain.doFilter(request,response);
+        return true;
     }
 }
