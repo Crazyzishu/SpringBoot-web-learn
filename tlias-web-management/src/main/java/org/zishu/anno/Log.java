c1d6d2c555066c591872c0b2309efd4ab9035dcf
zishu <129256950+Crazyzishu@users.noreply.github.com>
2025-07-11 18:45:59 +0800

    update

--- tlias-web-management/src/main/java/org/zishu/anno/Log.java
+++ tlias-web-management/src/main/java/org/zishu/anno/Log.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 @Target(ElementType.METHOD)//指定此注解在哪生效 -- 只能加在方法上
 @Retention(RetentionPolicy.RUNTIME)//指定此注解运行时生效
 public @interface Log {
